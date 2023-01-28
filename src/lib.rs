314b9f1ad7cbedc00acf080e5b5397f9481c4728
Maikon Weber <m.carvalho@grouplinknetwork.com>
2023-01-28 18:19:47 -0300

    Add Bevy_renet network from scrata

---
+++ src/lib.rs
@@ -0,0 +1,81 @@
+#[allow(unused)]
+pub use bevy::prelude::*;
+pub use bevy_renet::renet::*;
+pub use bevy_renet::*;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// use bevy::{
+//     prelude::*,
+//     render::camera::ScalingMode 
+// };
+
+// pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+// pub const RESOLUTION: f32 = 16.0/9.0;
+// pub const TILE_SIZE : f32 = 0.10;
+
+
+// fn main() {
+//     let height: f32 = 900.0;
+//     App::new() 
+//     .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+//     .add_plugins(DefaultPlugins.set(WindowPlugin {
+//         window : WindowDescriptor {
+//             title: "I am a Window".to_string(),
+//                     width: height * RESOLUTION,
+//                     height: height,
+//                     resizable: true,
+//                     ..Default::default()        
+//         },
+//         ..default()
+//     }))
+    
+//     // My Setup
+
+
+//     // .add_plugins(DefaultPlugins.set(WindowPlugin {
+//     //     window: WindowDescriptor {
+//     //         title: "I am a Window".to_string(),
+//     //         width: 1600.0,
+//     //         height: 800.0,
+//     //         resizable: true,
+//     //         ..Default::default()
+//     //     },
+//     //     ..default()
+//     // }))
+
+//     .run();       
+// }
+
+// fn spawn_camera(mut commands: Commands) {    
+
+// }
+
+
+
+
+
+// fn hello_word() {
+//     println!("Hello Word")
+// }
+
+
+// fn add_people (mut commands: Commands) {
+//     commands.spawn(Person);
+// }
+
+
+
