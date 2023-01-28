2e21073797851c4e86186fab07b0bbcc96959456
Maikon Weber <m.carvalho@grouplinknetwork.com>
2023-01-28 16:00:24 -0300

    Add Janela to start game

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 #[allow(unused)]
 
 use bevy::prelude::*;
+use bevy::{prelude::Commands, input::InputPlugin};
+
+
+#[derive(Component)]
+
+struct Person;
+
+struct Name(String);
 
 
 fn main() {
     App::new()
+    .add_plugins(DefaultPlugins.set(WindowPlugin {
+        window: WindowDescriptor {
+            title: "I am a Window".to_string(),
+            width: 1200.0,
+            height: 1600.0,
+            ..default()
+        },
+        ..default()
+    }))
+    .add_startup_system(add_people)
     .add_system(hello_word)
     .run();   
 }
@@ -15,6 +33,9 @@ fn hello_word() {
 }
 
 
+fn add_people (mut commands: Commands) {
+    commands.spawn(Person);
+}
 
 
 
