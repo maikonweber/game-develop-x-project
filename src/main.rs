ad1aa2d5a28dd1015138807a15c8123727b10a1e
Maikon Weber <m.carvalho@grouplinknetwork.com>
2023-01-29 15:09:59 -0300

    Add yew bug with trunk

--- src/main.rs
+++ src/main.rs
@@ -1,120 +1,17 @@
-use bevy::{prelude::*, render::camera::ScalingMode,
-input::keyboard::*,
-};
+use yew::prelude::*;
 
 
+#[function_component]
+fn App() -> Html {
+    return html! {
+        <>
+        <div> { "Hello Word" } </div>
+        </>
+    };
+}
+
+// We load "App" and render it.
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .run();
-}
-
-/// set up a simple 3D scene
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // camera
-    commands.spawn(Camera3dBundle {
-        projection: OrthographicProjection {
-            scale: 3.0,
-            scaling_mode: ScalingMode::FixedVertical(2.0),
-            ..default()
-        }
-        .into(),
-        transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
-
-    // plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
-    // cubes
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(1.5, 0.5, 1.5),
-        ..default()
-    });
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(1.5, 0.5, -1.5),
-        ..default()
-    });
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(-1.5, 0.5, 1.5),
-        ..default()
-    });
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(-1.5, 0.5, -1.5),
-        ..default()
-    });
-    // light
-    commands.spawn(PointLightBundle {
-        transform: Transform::from_xyz(3.0, 8.0, 5.0),
-        ..default()
-    });
-}
-
-
-fn keyboard_input(keys: Res<Input<KeyCode>>) {
-    if keys.just_pressed((KeyCode::Space)) {}
-    if keys.just_pressed((KeyCode::LControl)) {}
-    if keys.pressed(KeyCode::W) {}
-}
-
-
-fn keyboard_events(
-    mut key_evr: EventReader<KeyboardInput>,
-) {
-    use bevy::input::ButtonState;
-
-    for ev in key_evr.iter() {
-        match ev.state {
-            ButtonState::Pressed => {
-                println!("Key press: {:?} ({})", ev.key_code, ev.scan_code);
-            }
-            ButtonState::Released => {
-                println!("Key release: {:?} ({})", ev.key_code, ev.scan_code);
-            }
-        }
-    }
-}
-
-
-fn mouse_button_input(
-    buttons: Res<Input<MouseButton>>,
-) {
-    if buttons.just_pressed(MouseButton::Left) {
-        // Left button was pressed
-    }
-    if buttons.just_released(MouseButton::Left) {
-        // Left Button was released
-    }
-    if buttons.pressed(MouseButton::Right) {
-        // Right Button is being held down
-    }
-    // we can check multiple at once with `.any_*`
-    if buttons.any_just_pressed([MouseButton::Left, MouseButton::Right]) {
-        // Either the left or the right button was just pressed
-    }
-}
-
-
-fn mouse_motion(
-    mut motion_evr: EventReader<MouseMotion>,
-) {
-    for ev in motion_evr.iter() {
-        println!("Mouse moved: X: {} px, Y: {} px", ev.delta.x, ev.delta.y);
-    }
+    #[cfg(feature = "render")]
+    yew::Renderer::<App>::new().render();
 }
