9e44ece36545f0a5a4bb60faf3da1d62f26fd2c6
Maikon Weber <m.carvalho@grouplinknetwork.com>
2023-01-30 16:01:35 -0300

    Add yew

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,61 @@ use bevy::{prelude::*, render::camera::ScalingMode,
 };
 use bevy::core_pipeline::clear_color::ClearColorConfig;
 
+
+
+use yew::prelude::*;
+use stylist::{css, Style};
+use yew::prelude::*;
+
+#[function_component]
+fn Appy() -> Html {    
+    let s = css!(
+        r#"
+            width: 100%;
+            background-color: red;
+            height: 75px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 10px;
+            color : ${color};
+            span, ${sel_div} {
+                background-color: blue;
+
+            }
+
+        
+            @media screen and ${breakpoint} {
+                display: flex;
+
+            }
+        "#,
+        color = "black",
+        sel_div = "div.selected",
+        breakpoint = "(max-width: 500px)",
+    );
+
+    let style = Style::new(s).expect("msg");
+
+    return html! {
+
+        <>
+      
+        <div class={style}> { "Hello Word"} 
+        </div>
+
+        </>
+    };
+}
+
+// We load "App" and render it.
+
+
+
 fn main() {
+    yew::Renderer::<Appy>::new().render();
     App::new()
-        .add_plugins(DefaultPlugins)
+    .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .run();
 }
